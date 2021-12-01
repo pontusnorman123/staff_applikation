644dabcfb77cc93802027e1809051d00084399a9
Marcus Collard <marcuscollard225@gmail.com>
2021-12-01 17:37:49 +0100

    marcus 3

--- app/src/main/java/com/example/staff_app/Food.java
+++ app/src/main/java/com/example/staff_app/Food.java
@@ -1,5 +1,7 @@
 package com.example.staff_app;
 
+import android.util.Log;
+
 public class Food {
 
     private String name;
@@ -9,7 +11,10 @@ public class Food {
         this.name = name;
     }
 
-    public void incrementCounter() {counter++; }
+    public void incrementCounter() {
+        counter = counter + 1;
+        Log.d("Food", "counter incremented to: " + counter);
+    }
 
     public int getCount() {return counter;}
     public String getName() {return name;}
