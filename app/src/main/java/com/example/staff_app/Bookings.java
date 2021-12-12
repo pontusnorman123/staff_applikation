ba431024db0353fbc7b84f475a285c2691430362
Gromzie <max39032@live.se>
2021-12-12 15:14:00 +0100

    Påväg med boknings meny

--- app/src/main/java/com/example/staff_app/Bookings.java
+++ app/src/main/java/com/example/staff_app/Bookings.java
@@ -2,13 +2,8 @@ package com.example.staff_app;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-import android.os.Handler;
 
-import java.util.ArrayList;
 
 public class Bookings extends AppCompatActivity {
 
@@ -17,31 +12,5 @@ public class Bookings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
 
-
-        // FROM HARDCODED TEST ARRAY
-        SO.s.resetAll();
-        ArrayList<Order> test_orders = new ArrayList<Order>(5);
-        test_orders.add(new Order(1));
-        test_orders.add(new Order(3));
-        test_orders.add(new Order(5));
-        test_orders.add(new Order(3));
-        test_orders.add(new Order(2));
-
-        RecyclerView recycler = findViewById(R.id.recycler);
-
-        //SO.s.context = UndeliveredOrders.this;
-
-        SO.s.customAdapter = new OrdersCustomAdapter(Bookings.this);
-        recycler.setAdapter(SO.s.customAdapter);
-        recycler.setLayoutManager(new LinearLayoutManager(Bookings.this));
-
-        SO.s.addOrders(test_orders);
-
-
-        // FROM DATABASE
-        /*XmlReaderTask xmlReaderTask = new XmlReaderTask();
-        xmlReaderTask.tableList = null;
-        xmlReaderTask.handler = new Handler();//Håller  koll på trådsom är ansvar för  nätverk
-        xmlReaderTask.execute();*/
     }
 }
